028619e8a6399a42b4481c8fcbbe5d189c9ae917
Gazi <Gazi@DESKTOP-224SE1B>
2024-03-26 16:22:19 +0300

    ClassLibrary-StaticKavrami

--- Sube1.EmlakciApp/Program.cs
+++ Sube1.EmlakciApp/Program.cs
@@ -1,80 +1,41 @@
-﻿namespace Sube1.EmlakciApp
+﻿using Sube1.EmlakciLib;
+
+namespace Sube1.EmlakciApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Ev evim;//Referans
-            //evim = new Ev();//Ev classından nesne türetildi.
-            //evim.alan = 100;
-            //evim.odasayisi = 2;
-            //evim.katno = 1;
-            //evim.semt = "Gazi";
+            #region Catch NullException
+            //try
+            //{
+            //    Ev evim;//Referans
+            //    evim = new Ev();//Ev classından nesne türetildi.
+            //    evim.alan = 100;
+            //    evim.odasayisi = 2;
+            //    evim.katno = 1;
+            //    evim.semt = "Gazi";
+            //    EvBilgileri(evim);
+            //}
+            //catch (NullReferenceException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch(Exception)
+            //{
+            //    Console.WriteLine("Bir hata oluştu");
+            //} 
+            #endregion            
 
-            //string bilgi = EvBilgileri(evim);
-            //Console.WriteLine(bilgi);
+            Ev evim2 = new Ev();
+            evim2.alan = 120;
+            evim2.katno = 2;
+            evim2.semt = "Kazan";
+            evim2.odasayisi = 3;
 
-            //Ev evim2 = new Ev();
-            //evim2.alan = 120;
-            //evim2.katno = 2;
-            //evim2.semt = "Kazan";
-            //evim2.odasayisi = 3;
-
-            //Console.WriteLine(EvBilgileri(evim2));
-
-            try
-            {
-                Ev evim3 = null;
-                Console.WriteLine(EvBilgileri(evim3));
-            }
-            catch (NullReferenceException)
-            {
-                Console.WriteLine("Referans null geldi!");
-            }
-            catch(Exception)
-            {
-                Console.WriteLine("Bilinmeyen Hata!");
-            }
+            Console.WriteLine(evim2.EvBilgileri());
         }
-
-        static string EvBilgileri(Ev home)
-        {
-            //if (home != null)//Null check
-            //{
-            //    return $"Oda Sayısı:{home.odasayisi}\nKat No:{home.katno}\nAlan:{home.alan}\nSemt:{home.semt}";
-            //}
-            //else
-            //{
-            //    return "Referans Null geldi!";
-            //}
-
-            //if (home != null)//Null check
-            //{
-            //    return $"Oda Sayısı:{home.odasayisi}\nKat No:{home.katno}\nAlan:{home.alan}\nSemt:{home.semt}";
-            //}
-            //else
-            //{
-            //    throw new NullReferenceException();
-            //}
-
-            try
-            {
-                return $"Oda Sayısı:{home.odasayisi}\nKat No:{home.katno}\nAlan:{home.alan}\nSemt:{home.semt}";
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-    }
-
-    class Ev
-    {
-        public int odasayisi;//field
-        public int katno;
-        public string semt;
-        public double alan;
-    }
+    }   
 }
 
 //Classlar aynı zamanda bir veri tipidir.
@@ -82,3 +43,9 @@
 //Bir classtan nesne oluşturulduğunda, class içindeki fieldlara varsayılan değerleri atanır.
 //private olarak tanımlanan üyelere, class dışından erişilemez.
 //public olarak tanımlanan üyelere class dışından erişilebilir.
+//Write Once Use Everywhere!
+//OOP: Object Oriented Programming
+//Class içerisinde static olarak tanımlanan üyeler, class'a ait olurlar ve bu üyelere class adı ile erişilebilir.
+//Static olmayan üyeler ise nesneye aittir ve nesne referansı ile erişilebilir.
+
+//DRY- Don't Repeat Yourself
---
+++ Sube1.EmlakciLib/Ev.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Sube1.EmlakciLib
+{
+    public class Ev
+    {
+        public int odasayisi;//field
+        public int katno;
+        public string semt;
+        public double alan;
+
+        public string EvBilgileri()
+        {
+            return $"Oda Sayısı:{this.odasayisi}\nKat No:{this.katno}\nAlan:{this.alan}\nSemt:{this.semt}";
+
+            #region Try-Catch-NullCheck
+            //if (home != null)//Null check
+            //{
+            //    return $"Oda Sayısı:{home.odasayisi}\nKat No:{home.katno}\nAlan:{home.alan}\nSemt:{home.semt}";
+            //}
+            //else
+            //{
+            //    return "Referans Null geldi!";
+            //}
+
+            //try
+            //{
+            //    return $"Oda Sayısı:{home.odasayisi}\nKat No:{home.katno}\nAlan:{home.alan}\nSemt:{home.semt}";
+            //}
+            //catch (Exception ex)
+            //{                
+            //    throw;
+            //} 
+            #endregion
+        }
+    }
+}
